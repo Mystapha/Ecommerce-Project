6d0fe80c1c6a80a88ffee5c31905ade6734ee049
Mystapha <moustafaamara@gmail.com>
2024-07-29 10:23:20 +0100

    second commit

--- API/index.js
+++ API/index.js
@@ -1,6 +1,6 @@
 const express=require("express");
 const  cors=require("cors");
-const connectDB=require('./config/db_connect');
+const connectDB=require("./config/db_Connect");
 const app = express();
 require('dotenv').config();
 
@@ -16,7 +16,7 @@ app.use(cors());
 app.use("/user",require("./routes/user"));
 app.use("/product", require("./routes/product"));
 app.use("/commande", require("./routes/commande"));
-app.get("/",(req,res)=>res.send("Exp"))
+app.get("/",(req,res)=>res.send("Express on Vercel"));
 //server
 const PORT=process.env.PORT || 5000 ;
 app.listen(PORT,(err)=> {err ?
